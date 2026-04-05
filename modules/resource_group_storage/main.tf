ed95e47df5a741420cbf1e16e57ad6e65559528b
Никита <Goldbergnik1993@gmail.com>
2026-04-05 19:32:14 +0200

    dev2

--- modules/resource_group_storage/main.tf
+++ modules/resource_group_storage/main.tf
@@ -1,26 +1,17 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "dev"
+    terraform   = "true"
   }
-}
-
-provider "azurerm" {
-  features {}
-}
-
-module "storage_account_final" {
-  source  = "goldbergnik1993-cloud/resource-group-storage/azurerm"
-  version = "1.0.0"
-
-  resource_group_name  = "rg-terraform-task"
-  location            = "West Europe"
-
-  storage_account_name = "goldbergstorage2026test" 
-}
-
-output "final_storage_id" {
-  value = module.storage_account_final.storage_account_id
 }
